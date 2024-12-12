d78febd4d1d129d287413692d5e5676cbecf52ed
mrfouu <bryanweinegaessel@gmail.com>
2024-12-12 08:57:15 +0100

    correcting betty style task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,9 +3,8 @@
 /**
  * binary_tree_sibling - Finds the sibling of a node in a binary tree.
  * @node: A pointer to the node to find the sibling of.
- *
  * Return: If node is NULL or there is no sibling - NULL.
- * 		Otherwise - a pointer to the sibling.
+ * Otherwise - a pointer to the sibling.
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
